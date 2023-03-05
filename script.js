81aa45e19aeb4595ed6fc2ea64aff08536feca52
HarshKhandelwal101 <78348500+HarshKhandelwal101@users.noreply.github.com>
2023-03-05 22:22:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 //your JS code here. If required.
+
+//your code here
+function describe(testSuiteName, func) { console.log(`beginning test suite ${testSuiteName}`);
+try {
+func();
+console.log(`successfully completed test suite ${testSuiteName}`);
+} catch (error) { const {testCaseName, errorMessage} = error; console.error(
+`failed running test suite ${testSuiteName} on `+
+`test case ${testCaseName} with error message ${errorMessage}`,
+);
+}
+}
+function it (testCaseName, func) {
+console.log(`beginning test case ${testCaseName}`);
+try {func();
+    console.log(`successfully completed test case ${testCaseName}`);
+    } catch(errorMessage) {
+    throw {testCaseName, errorMessage};
+    }
+    }
+    function expect(actual) { return new ExpectFunctions (actual)};
+    function ExpectFunctions (actual) { this.actual = actual;
+    this.stringifiedActual = JSON.stringify(actual);
+    }
+    ExpectFunctions.prototype.toExist = function () {
+    if (this.actual == null) {
+    throw `expected value to exist but got ${this.stringifiedActual}`;
+    }
+};
+ExpectFunctions.prototype.toBe = function (expected) {
+    if (this.actual !== expected) {
+    throw `expected ${this.stringifiedActual} to be ${JSON.stringify(expected)}`;
+    }
+    };
+    ExpectFunctions.prototype.toBeType = function (type) {
+    if (typeof this.actual !== type) {
+    throw `expected ${this.stringifiedActual} to be of type ${type} but got ${typeof this.actual}`;
+    }
+    };
+    // Do not edit the lines below.
+    exports.describe =describe;
+    exports.it = it;
+    exports.expect = expect;
+    
